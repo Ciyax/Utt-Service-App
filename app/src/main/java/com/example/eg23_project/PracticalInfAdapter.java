ff033898fecc4a0d543f897cf0cb1b4d567e2731
Albou-lab <albane.donat@gmail.com>
2020-06-15 14:15:23 +0200

    Création de l'onglet static map dans les informations utiles

--- app/src/main/java/com/example/eg23_project/PracticalInfAdapter.java
+++ app/src/main/java/com/example/eg23_project/PracticalInfAdapter.java
@@ -22,6 +22,9 @@ public class PracticalInfAdapter extends FragmentStatePagerAdapter {
                 fragment = new PracticalInfScheme();
                 break;
             case 2:
+                fragment = new PracticalInfStaticMap();
+                break;
+            case 3:
                 fragment = new PracticalInfHealth();
                 break;
         }
@@ -30,7 +33,7 @@ public class PracticalInfAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return 4;
     }
 
     @Override
@@ -45,6 +48,9 @@ public class PracticalInfAdapter extends FragmentStatePagerAdapter {
                 charSequence = "Plan de l\'UTT";
                 break;
             case 2:
+                charSequence = "Ville de Troyes";
+                break;
+            case 3:
                 charSequence = "Santé";
                 break;
         }
---
+++ app/src/main/java/com/example/eg23_project/PracticalInfStaticMap.java
@@ -0,0 +1,7 @@
+package com.example.eg23_project;
+
+import android.support.v4.app.Fragment;
+
+public class PracticalInfStaticMap extends Fragment {
+
+}
